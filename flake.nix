055bc55adf83f5209dd7654c78f423e75efaf8b8
Marla Albuquerque <marla@albuque.com>
2024-08-16 12:56:38 -0300

    chore: moves to git

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "s11c macos nix config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = { pkgs, ... }: {
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        with pkgs; [ 
+                neovim
+		slack
+		raycast
+		discord
+		jetbrains.webstorm
+		gh
+		jira-cli-go
+		lazygit
+        ];
+
+      users.users.marla.home = "/Users/marla";
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      nix = {
+        linux-builder.enable = true;
+	      extraOptions = ''
+		extra-platforms = x86_64-darwin aarch64-darwin
+	      '';
+      };
+
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      system.defaults = {
+        finder.AppleShowAllExtensions = true;
+      };
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."s11c-mac-studio" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ 
+      	configuration 
+        home-manager.darwinModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.marla = import ./home.nix;
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."s11c-mac-studio".pkgs;
+  };
+}
