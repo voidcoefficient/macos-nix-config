4f93da4b42929f45ad5e3979782bbc843570a1ec
Marla Albuquerque <marla@albuque.com>
2024-08-17 00:38:24 -0300

    feat(home/alacritty): font

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -14,14 +14,24 @@
         opacity = 0.8;
         blur = true;
 
-	dynamic_padding = false;
+        dynamic_padding = false;
         padding.x = 0;
         padding.y = 0;
         decorations = "buttonless";
       };
 
       font = {
-        size = 14.0;
+        size = 16.0;
+        normal = {
+          family = "Inconsolata Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "Inconsolata Nerd Font";
+        };
+        italic = {
+          family = "Inconsolata Nerd Font";
+        };
       };
     };
   };
